3fd9c6e12f57da8561629e05cf4616aeeeb681c6
ShaileshPrajapati-BTC <prajapatishailesh@botreetechnologies.com>
2017-04-18 17:02:51 +0530

    first commit with take snapshot

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :products,:path => 'Picture', only: [:new, :create, :index, :edit, :destroy]
+  root 'products#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
